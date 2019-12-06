68c920b0a89cc80878b7d49f47acaeb6926b7b77
Steven Mackey <stevester94@gmail.com>
2019-12-05 22:14:50 -0800

    Update some helper scripts

--- benchmarking/digging_deeper/get_proc_info.py
+++ benchmarking/digging_deeper/get_proc_info.py
@@ -3,25 +3,60 @@
 import psutil
 import sys
 import pprint
+import time
 
 pp = pprint.PrettyPrinter()
 
-procs = []
+pids_first = {}
+pids_last  = {}
 
-for proc in psutil.process_iter():
-    try:
-        pinfo = proc.as_dict(attrs=['name', 'num_ctx_switches'])
-        reduced = {
-            "name": pinfo["name"],
-            "voluntary_ctx_switches":pinfo['num_ctx_switches'][0]
-        }
+diffs = []
 
-        procs.append(reduced)
-    except psutil.NoSuchProcess:
-        pass
+def populate_pids_dict(d):
+    for proc in psutil.process_iter():
+        try:
+            pinfo = proc.as_dict(attrs=['pid', 'name', 'num_ctx_switches'])
+            reduced = {
+                "name": pinfo["name"],
+                "voluntary_ctx_switches":pinfo['num_ctx_switches'][0]
+            }
 
-procs.sort(key=lambda x: x["voluntary_ctx_switches"], reverse=True)
+            d[pinfo["pid"]] = reduced
+        except psutil.NoSuchProcess:
+            pass
+
+
+def test():
+    for proc in psutil.process_iter():
+        try:
+            pinfo = proc.as_dict()
+            if "openvpn" in pinfo["name"]:
+                pp.pprint(pinfo)    
+        except:
+            pass
+    sys.exit(0)
+
+if __name__ == "__main__":
+
+    if sys.argv[1] == "TEST": test()
+
+    populate_pids_dict(pids_first)
+    time.sleep(int(sys.argv[1]))
+    populate_pids_dict(pids_last)
+
+    # Get how many context switches ocurred for each pid
+    for pid, val in pids_last.items():
+        try:
+            diff = {}
+            diff["name"] = val["name"]
+            diff["voluntary_ctx_switches_difference_per_sec"] = (val["voluntary_ctx_switches"] - pids_first[pid]["voluntary_ctx_switches"]) / int(sys.argv[1])
+
+            diffs.append(diff)
+        except:
+            pass
+
+diffs.sort(key=lambda x: x["voluntary_ctx_switches_difference_per_sec"], reverse=True)
 # pp.pprint(procs[0:10])
 
-for p in procs[0:10]:
+for p in diffs[0:10]:
     print(p)
---
+++ benchmarking/sync.bash
@@ -0,0 +1,2 @@
+rsync -r digging_deeper/ 192.168.86.100:~/CSC255_Project/benchmarking/digging_deeper/
+rsync -r digging_deeper/ 192.168.86.200:~/CSC255_Project/benchmarking/digging_deeper/
